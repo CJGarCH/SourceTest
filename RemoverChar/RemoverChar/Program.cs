2f100cdb90341e6354834cdd34ec67b56856b322
CJGarCH <47647416+CJGarCH@users.noreply.github.com>
2022-10-02 01:02:44 -0500

    Add files via upload

---
+++ RemoverChar/RemoverChar/Program.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+ class RemoveCharacter {
+   static void Main() {
+
+     string mensaje = "CarlosAlbertoLopezJaramillo";
+        // int size = s.Length;
+
+        if (mensaje.Length >= 20)
+        {
+            mensaje = mensaje.Remove(20, 7).Insert(20, "...");
+            Console.WriteLine("El mensaje tiene más de 20 carácteres, será modificado: \n" + mensaje);
+
+
+        }
+        else
+        {
+            Console.WriteLine("El arreglo no tiene más de 20 carácteres por lo tanto no" +
+                "va a ser modificado" + mensaje);
+
+        }
+
+    }
+ }
