a14d887c5422d344cea6bb240a5ec71e5cf212fa
CJGarCH <47647416+CJGarCH@users.noreply.github.com>
2022-10-02 01:07:16 -0500

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci 
+{
+    internal class Program
+    {
+        static bool CuadradoPerfecto(int ex)
+        {
+            int s = (int)Math.Sqrt(ex);
+
+            return (s * s == ex);
+        }
+        static bool esNacci(int n)
+        {
+            return CuadradoPerfecto(5 * n * n + 4) || CuadradoPerfecto(5 * n * n - 4);
+        }
+        static void Main(string[] args)
+        {
+            int n = 3;
+
+            Console.WriteLine(esNacci(n) ? n + " \nEs parte de la serie" : n + " \nNo es parte de la serie");
+        }
+    }
+}
